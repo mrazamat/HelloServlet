94fb192a4e85739782f2fb578b3fdeb5acf11acb
Azamat <azamat00189@mail.ru>
2018-12-25 20:53:50 +0500

    Added any methods

---
+++ src/uz/coder/model/BeerExpert.java
@@ -0,0 +1,18 @@
+package uz.coder.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BeerExpert {
+    public List<String> getBrands(String color) {
+        List<String> brands = new ArrayList<>();
+        if (color.equals("ambar")) {
+            brands.add("Jack Amber");
+            brands.add("Red Moose");
+        } else {
+            brands.add("Jail Pale Ale");
+            brands.add("Gout Stout");
+        }
+        return brands;
+    }
+}
