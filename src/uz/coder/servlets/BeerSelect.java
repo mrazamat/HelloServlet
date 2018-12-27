86bcb9a259672005718086611ea0fde7b392e926
Azamat <azamat00189@mail.ru>
2018-12-27 22:06:58 +0500

    Servlets and JSP

--- src/uz/coder/servlets/BeerSelect.java
+++ src/uz/coder/servlets/BeerSelect.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+import static java.lang.System.out;
+
 @WebServlet(name = "BeerSelect")
 public class BeerSelect extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -18,17 +20,13 @@ public class BeerSelect extends HttpServlet {
         BeerExpert be = new BeerExpert();
         List result = be.getBrands(c);
 
-//        response.setContentType("text/html");
-//        PrintWriter out = response.getWriter();
-//        out.println("Beer Selection Advice<br>");
+
+
+
         request.setAttribute("styles", result);
         RequestDispatcher view = request.getRequestDispatcher("result.jsp");
         view.forward(request, response);
         Iterable<String> it = (Iterable) result.iterator();
-
-//        while (((Iterator) it).hasNext()) {
-//            out.println("<br>try: " + ((Iterator) it).next());
-//        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- web/result.jsp
+++ web/result.jsp
@@ -1,5 +1,6 @@
 <%@ page import="java.util.Iterator" %>
 <%@ page import="java.util.List" %>
+<%@ page import="static java.lang.System.out" %>
 <%@ page contentType="text/html;charset=UTF-8" %>
 <html>
 <head>
@@ -10,9 +11,20 @@
 <%
     List styles = (List) request.getAttribute("styles");
     Iterator it = styles.iterator();
-    while (it.hasNext()){
+    while (it.hasNext()) {
         out.print("<br> try : " + it.next());
     }
+    String[] sizes = request.getParameterValues("sizes");
+    for (int i = 0; i < sizes.length; i++) {
+        out.print("<br>sizes: " + sizes[i]);
+    }
+    String client = request.getHeader("User-Agent");
+    Cookie[] cookie = request.getCookies();
+    HttpSession
+    out.print("<br>");
+    out.print("User-Agent: " + client);
+    out.print("<br>");
+    out.print("<br>" + cookie[0]);
 %>
 </body>
 </html>
